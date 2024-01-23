fa7a03787281a13463a71823338b52f666cc9919
maximpy2k <maximpy2k@gmail.com>
2024-01-23 11:16:23 +0300

    Создан проект

---
+++ StationImitation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StationImitation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
